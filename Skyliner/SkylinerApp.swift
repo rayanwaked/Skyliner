115311de0c8c6e5fb656bca06d2d9b23ba240b18
Rayan Waked <rayan@waked.dev>
2025-07-13 17:21:37 -0700

    Initial Commit

---
+++ Skyliner/SkylinerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkylinerApp.swift
+//  Skyliner
+//
+//  Created by Rayan Waked on 7/13/25.
+//
+
+import SwiftUI
+
+@main
+struct SkylinerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
